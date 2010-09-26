4a7d00a1687328e2ad1c056d4f27c44e9a01a2ec
Willington Vega <wvega@wvega.com>
2010-09-26 16:38:50 -0500

    Step 7.

--- project/websetup/bootstrap.py
+++ project/websetup/bootstrap.py
@@ -11,6 +11,46 @@ import transaction
 def bootstrap(command, conf, vars):
     """Place any commands to setup project here"""
 
-    # <websetup.bootstrap.before.auth
+    # <websetup.bootstrap.before.auth>
+    
+    from sqlalchemy.exc import IntegrityError
+
+    try:
+        u = model.User()
+        u.name = u'Example manager'
+        u.email = u'manager@somedomain.com'
+        u.password = u'managepass'
+
+        model.DBSession.add(u)
+
+        g = model.Group()
+        g.slug = u'managers'
+        g.name = u'Managers Group'
+
+        g.users.append(u)
+
+        model.DBSession.add(g)
+
+        p = model.Permission()
+        p.name = u'manage'
+        p.description = u'This permission give an administrative right to the bearer'
+        p.groups.append(g)
+
+        model.DBSession.add(p)
+
+        editor = model.User()
+        editor.name = u'Example editor'
+        editor.email = u'editor@somedomain.com'
+        editor.password = u'editpass'
+
+        model.DBSession.add(editor)
+        model.DBSession.flush()
+        transaction.commit()
+    except IntegrityError:
+        print 'Warning, there was a problem adding your auth data, it may have already been added:'
+        import traceback
+        print traceback.format_exc()
+        transaction.abort()
+        print 'Continuing with bootstrapping...'
 
     # <websetup.bootstrap.after.auth>
